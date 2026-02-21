00f52e99f754b5dafe5016c6f4cd49b96b762bdf
BEANMAN5001 <255559543+BEANMAN5001@users.noreply.github.com>
2026-02-21 11:56:23 -0600

    i did camra.

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -11,6 +11,7 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
   // AprilTag layout
@@ -20,13 +21,26 @@ public class VisionConstants {
   // Camera names, must match names configured on coprocessor
   public static String camera0Name = "VisionCam1";
   public static String camera1Name = "VisionCam2";
-
+  public static String camera2Name = "VisionCam4";
+  public static String camera3Name = "VisionCam6";
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
-      new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI / 2));
+      new Transform3d(0, 0.3, 0.2, new Rotation3d(0.0, 0.4, Math.PI / 2));
   public static Transform3d robotToCamera1 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI / 2 * 3));
+      new Transform3d(0, 0.13, 0.2, new Rotation3d(0.0, 0.4, Math.PI / 2));
+  public static Transform3d robotToCamera2 =
+      new Transform3d(
+          Units.inchesToMeters(11),
+          Units.inchesToMeters(-12.75),
+          Units.inchesToMeters(13 + (7 / 8)),
+          new Rotation3d(0.0, 0, (7 * Math.PI) / 4));
+  public static Transform3d robotToCamera3 =
+      new Transform3d(
+          Units.inchesToMeters(-12),
+          Units.inchesToMeters(12 + (3 / 4)),
+          Units.inchesToMeters(12 + (7 / 8)),
+          new Rotation3d(0, Math.PI / 4, (3 * Math.PI) / 2));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
