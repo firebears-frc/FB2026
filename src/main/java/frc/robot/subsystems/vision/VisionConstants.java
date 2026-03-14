70bd0c9a06984ea37cc59b60e20dd9fafe51b59a
MartijnKM <Martijnkrookmagnuson@gmail.com>
2026-03-14 11:02:17 -0500

    x, y, z offsets of the cameras updated

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -27,28 +27,28 @@ public class VisionConstants {
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
       new Transform3d(
-          Units.inchesToMeters(10.75),
-          Units.inchesToMeters(-12.625),
-          Units.inchesToMeters(14.25),
+          Units.inchesToMeters(8.5),
+          Units.inchesToMeters(13.6275),
+          Units.inchesToMeters(13.5625),
           new Rotation3d(
               Units.degreesToRadians(12), Units.degreesToRadians(-4), Units.degreesToRadians(296)));
   public static Transform3d robotToCamera1 =
       new Transform3d(
-          Units.inchesToMeters(-11.5),
-          Units.inchesToMeters(-12.5625),
-          Units.inchesToMeters(14),
+          Units.inchesToMeters(-11.25),
+          Units.inchesToMeters(12.9375),
+          Units.inchesToMeters(16.625),
           new Rotation3d(0, Units.degreesToRadians(-18), Units.degreesToRadians(225)));
   public static Transform3d robotToCamera2 =
       new Transform3d(
-          Units.inchesToMeters(8.625),
-          Units.inchesToMeters(13.875),
-          Units.inchesToMeters(14.625),
+          Units.inchesToMeters(11),
+          Units.inchesToMeters(-12.375),
+          Units.inchesToMeters(14.375),
           new Rotation3d(0, 0, Units.degreesToRadians(90)));
   public static Transform3d robotToCamera3 =
       new Transform3d(
-          Units.inchesToMeters(-11.75),
-          Units.inchesToMeters(12.875),
-          Units.inchesToMeters(14.3125),
+          Units.inchesToMeters(-11.5),
+          Units.inchesToMeters(-12.75),
+          Units.inchesToMeters(16.625),
           new Rotation3d(0, Units.degreesToRadians(-26), Units.degreesToRadians(135)));
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
